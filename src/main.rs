d6df323fc236c0b0f51b902dfd05f261aecfac2b
mc962 <11969409+mc962@users.noreply.github.com>
2020-06-30 20:51:07 -0400

    Move main code into separate function.

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,16 @@ mod client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let random_facts = client::get_random_facts(1).await?;
+    run().await;
+    Ok(())
+}
+
+async fn run() {
+    let random_facts = client::get_random_facts(1).await.unwrap();
     let random_fact = random_facts.first();
 
     match random_fact {
         Some(fact) => println!("{}", fact.text),
         None => println!("A cat has swiped your facts")
     }
-    Ok(())
 }
